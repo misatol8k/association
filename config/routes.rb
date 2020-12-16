cfc48868e90dc4750786783aa7e689544cbfc389
misatol8k <misatolkurihara@gmail.com>
2020-12-16 14:10:36 +0900

    ブログ機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/blogs', to: 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
 end
